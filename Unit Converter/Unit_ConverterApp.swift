794c44c4c85d0d258745960ef804bbf543690dbe
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-11-15 21:36:34 -0500

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Kyle Housel on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
